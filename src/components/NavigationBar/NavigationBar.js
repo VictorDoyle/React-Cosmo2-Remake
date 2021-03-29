750906a21dcd10688b909a2cc2b8fe1566ccd3d4
victordoyle <victor-doyle@outlook.com>
2021-03-28 22:24:59 -0400

    fixed navbar styling and added svgs for icons

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,12 +1,22 @@
 import React from 'react';
+
+/* styling */
+import '../../styles/organisms/NavigationBar.css';
+/* components */
 import CartIcon from './CartIcon';
+import MainLogo from './MainLogo';
+import MenuIcon from './MenuIcon';
+
+/* onMount color=transparent, on vh change to background: #1e1e1e; */
 
 function NavigationBar() {
     return(
         <>
-        <div className="leftNav"> <CartIcon /> </div>
-        <div className="midNav"></div>
-        <div className="rightNav"></div>
+        <div className="navigationGrid" id="navigationBarMain">
+            <div className="leftNav"> <CartIcon /> </div>
+            <div className="midNav"> <MainLogo /> </div>
+            <div className="rightNav"> <MenuIcon /> </div>
+        </div>
         </>
     )
 }
